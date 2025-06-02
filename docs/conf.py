1cd62a0581d1ee39e343764b6b936d5b5cc6b700
Alexandre Gràcia <alexusgracia@users.noreply.github.com>
2025-06-02 14:32:28 +0000

    Update 3 files
    - /index.md
    - /docs/conf.py
    - /.gitlab-ci.yml

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-
 import os
 import sys
 
-# Incluye la raíz del proyecto (donde están los .md)
+# Añadir raíz del proyecto al path
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
@@ -13,21 +11,16 @@ author = 'Alexandre Gràcia'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-extensions = [
-    'myst_parser',
-]
+extensions = ['myst_parser']
 
-# Permitir archivos Markdown
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 
-# Usa index.md como punto de entrada
 master_doc = 'index'
-
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
+# -- HTML output -------------------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
---
+++ index.md
@@ -0,0 +1,18 @@
+# Apunts PRO1
+
+Aquesta documentació conté els laboratoris de PRO1.
+
+## Contingut
+
+- [Planificació](planificacio.md)
+- [Laboratori 1](lab1.md)
+- [Laboratori 2](lab2.md)
+- [Laboratori 3](lab3.md)
+- [Laboratori 4](lab4.md)
+- [Laboratori 5](lab5.md)
+- [Laboratori 6](lab6.md)
+- [Laboratori 7](lab7.md)
+- [Laboratori 8](lab8.md)
+- [Laboratori 9](lab9.md)
+- [Laboratori 10](lab10.md)
+- [Laboratori 11](lab11.md)
