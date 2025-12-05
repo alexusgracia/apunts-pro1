c92aa520850b17330880deaaba2f44baf7f0d47a
Alexandre Gràcia Calvo <alexusgracia@users.noreply.github.com>
2025-12-05 19:32:29 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -24,3 +24,7 @@ exclude_patterns = []
 # -- HTML output -------------------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
